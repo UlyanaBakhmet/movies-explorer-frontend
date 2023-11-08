5811b5b6c499e20b6e8bff55dc3cdb4a02265307
Ulyana Bakhmet <buyarova.u@yandex.ru>
2023-11-08 15:48:30 +0300

    add 2 files

---
+++ src/utils/MainApi.js
@@ -0,0 +1,79 @@
+class MainApi {
+    constructor({baseUrl}) {
+      this._baseUrl = baseUrl;
+    }
+  
+    _checkResult(res) {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка ${res.status}`);
+    }
+  
+    addMovie(movieData) {
+      const token = localStorage.getItem('token');
+      return fetch(`${this._baseUrl}/movies`, {
+        method: "POST",
+        headers: {
+          "authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...movieData,
+        }),
+      }).then((res) => this._checkResult(res));
+    }
+  
+    deleteMovie(movieId) {
+      const token = localStorage.getItem('token');
+      return fetch(`${this._baseUrl}/movies/${movieId}`, {
+        method: "DELETE",
+        headers: {
+          "authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => this._checkResult(res));
+    }
+  
+    getSavedMovies() {
+      const token = localStorage.getItem('token');
+      return fetch(`${this._baseUrl}/movies`, {
+        method: "GET",
+        headers: {
+          "authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => this._checkResult(res));
+    }
+  
+    getUserInfo() {
+      const token = localStorage.getItem('token');
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: "GET",
+        headers: {
+          "authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }).then((res) => this._checkResult(res));
+    }
+  
+    editUserInfo({ name, email }) {
+      const token = localStorage.getItem('token');
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: {
+          "authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+        }),
+      }).then((res) => this._checkResult(res));
+    }
+  }
+  
+  export const mainApi = new MainApi({
+    baseUrl: "http://localhost:4000",
+    // baseUrl: "https://api.bakhmet-movies.nomoredomainsmonster.ru",
+  });
---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,26 @@
+class MoviesApi {
+    constructor({baseUrl}) {
+      this._baseUrl = baseUrl;
+    }
+  
+    _checkResult(res) {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка ${res.status}`);
+    }
+  
+    getMovies() {
+        return fetch(this._baseUrl, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => {
+        return this._checkResult(res);
+      });
+    }
+  }
+  
+  export const moviesApi = new MoviesApi({
+    baseUrl: "https://api.nomoreparties.co/beatfilm-movies",
+  });   
